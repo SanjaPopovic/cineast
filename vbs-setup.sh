87190e412ad3a0c95b90c56a6cceb487d5dfb977
Silvan Heller <silvan.heller@unibas.ch>
2020-09-23 17:42:47 +0200

    VBS 2021 Start

---
+++ vbs-setup.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# use ./vbs_setup.sh /tank/import/ 3 (3 being the number of threads)
+./gradlew cineast-runtime:fatJar
+./gradlew cineast-api:fatJar
+
+timestamp(){
+  date +"%m-%d_%H-%M-%S"
+}
+
+# restart cottontail and start a new tmux session with cottontail dumping to a new logfile
+restart_cottontail () {
+  echo "restarting cottontail"
+  kill $(pgrep --full cottontail)
+  cd ..
+  cd cottontaildb
+  tmux new-window -d -n cottontaildb "./gradlew run >> cottontail_$(timestamp).log"
+  cd ..
+  cd cineast
+}
+
+# base-folder
+base=$1
+threads=$2
+sleep=5
+currtime=$(timestamp)
+# clean logfiles
+rm *.log
+rm ../cottontaildb/*.log
+restart_cottontail
+sleep $sleep
+echo "setting up"
+java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json setup --clean >> vbs_setup_$currtime.log
+echo "importing proto files"
+java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json import --no-finalize --type PROTO --input $base/extracted_combined/ --batchsize 400 --threads $threads >> proto_import_$currtime.log
+echo "importing text & metadata"
+java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json import --no-finalize --type AUDIOTRANSCRIPTION --input $base/text/audiomerge.json --threads $threads --batchsize 15000 >> audio_import_$currtime.log
+java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json import --no-finalize --type CAPTIONING --input $base/text/captions.json --threads $threads >> captioning_import_$currtime.log
+java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json import --no-finalize --type GOOGLEVISION --input $base/text/gvision.json --threads $threads >> gvision_import_$currtime.log
+java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json import --no-finalize --type METADATA --input $base/text/metamerge.json --threads $threads --batchsize 25000 >> md_import_$currtime.log
+java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json import --no-finalize --type TAGS --input $base/text/V3C1Tags --threads $threads --batchsize 35000 >> tags_import_$currtime.log
+java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json import --no-finalize --type V3C1CLASSIFICATIONS --input $base/text/V3C1Analysis --threads $threads --batchsize 25000 >> text_import_$currtime.log
+java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json import --no-finalize --type V3C1FACES --input $base/text/V3C1Analysis/faces --threads $threads --batchsize 25000 >> text_import_$currtime.log
+java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json import --no-finalize --type OBJECTINSTANCE --input $base/features.csv --threads $threads --batchsize 400 >> mlt_import_$currtime.log
+
+sleep $sleep
+echo "optimizing"
+java -jar cineast-runtime/build/libs/cineast-runtime-2.5-full.jar vbs.json optimize >> vbs_optimize_$currtime.log
+sleep $sleep
+rm -r cineast_cache_*
+tmux new-window -d -n cineast "java -jar cineast-api/build/libs/cineast-api-3.0.1-full.jar vbs.json"
