4bbe63aa409612dc6823ecc2e402754b4221968c
SanjaPopovic <sanja.popovic@stud.unibas.ch>
2021-05-24 13:38:00 +0200

    Modify circle interface. Compatible with vitrivr-ng

--- cineast-core/src/main/java/org/vitrivr/cineast/core/data/Circle.java
+++ cineast-core/src/main/java/org/vitrivr/cineast/core/data/Circle.java
@@ -8,25 +8,33 @@ import java.util.List;
 
 public class Circle {
 
-    private final String type;
+    private final String type, semantic_name;
     private final double lat, lon, rad;
 
     /**
      * Constructor for {@link Circle}. Used to create object from JSON.
      *
-     * @param type       The type of the circle.
+     * @param type       The type indicates if a circle is drawn or if a location was given as tag. 'circle' or 'info'
      * @param lat        The latitude of the circle.
      * @param lon        The longitude of the circle.
      * @param rad        The radius of the circle.
+     * @param semantic_name The name of the location, when circe is drawn.
      */
     @JsonCreator
     public Circle(@JsonProperty(value = "type") String type,
                          @JsonProperty(value = "lat") double lat,
                          @JsonProperty(value = "lon") double lon,
-                         @JsonProperty(value = "rad") double rad) {
+                         @JsonProperty(value = "rad") double rad,
+                         @JsonProperty(value = "semantic_name") String semantic_name) {
         this.type = type;
         this.lat = lat;
         this.lon = lon;
         this.rad = rad;
+        this.semantic_name = semantic_name;
+        System.out.println("type = " + this.type);
+        System.out.println("lat = " + this.lat);
+        System.out.println("lon = " + this.lon);
+        System.out.println("rad = " + this.rad);
+        System.out.println("semantic_name = " + this.semantic_name);
     }
 }
