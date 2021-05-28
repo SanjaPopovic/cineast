90eb1a308aea8dc3dcade1bfc693a8f2ac8e6a8e
SanjaPopovic <sanja.popovic@stud.unibas.ch>
2021-05-28 13:11:04 +0200

    Merge distinct scoreElements

--- cineast-core/src/main/java/org/vitrivr/cineast/core/features/MapSearch.java
+++ cineast-core/src/main/java/org/vitrivr/cineast/core/features/MapSearch.java
@@ -1,5 +1,7 @@
 package org.vitrivr.cineast.core.features;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.vitrivr.cineast.core.config.QueryConfig;
 import org.vitrivr.cineast.core.config.ReadableQueryConfig;
 import org.vitrivr.cineast.core.data.Circle;
@@ -9,14 +11,16 @@ import org.vitrivr.cineast.core.db.DBSelectorSupplier;
 import org.vitrivr.cineast.core.features.abstracts.AbstractFeatureModule;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class MapSearch extends AbstractFeatureModule {
 
+    private static final Logger LOGGER = LogManager.getLogger();
     public static final String LOCATION_TABLE_NAME = "features_spatialdistance";
 
     public MapSearch() {
-        super(LOCATION_TABLE_NAME, 5000, 2);
+        super(LOCATION_TABLE_NAME, 10000, 2);
     }
 
     @Override
@@ -30,16 +34,27 @@ public class MapSearch extends AbstractFeatureModule {
         if (circles.isEmpty()) {
 
         }
-        List<ScoreElement> scoreElements = new ArrayList();
-        System.out.println(sc.getRegions());
+        LOGGER.debug(sc.getRegions());
+
+        HashMap<String, ScoreElement> distinctElements = new HashMap<>();
+        ReadableQueryConfig qc_new = new QueryConfig(qc).setDistanceIfEmpty(QueryConfig.Distance.haversine);
         for (Circle circle: circles) {
             double lat = circle.getLat();
             double lon = circle.getLon();
             float[] vec = {(float)lat, (float)lon};
-            ReadableQueryConfig qc_new = new QueryConfig(qc).setDistanceIfEmpty(QueryConfig.Distance.haversine);
-            scoreElements.addAll(getSimilar(vec, qc_new));
+            LOGGER.debug("Check fractionScoreElements");
+            List<ScoreElement> fractionScoreElements = getSimilar(vec, qc_new);
+            for (ScoreElement e: fractionScoreElements) {
+                distinctElements.put(e.getId(), e);
+                LOGGER.debug(e.getId());
+            }
+        }
+        List<ScoreElement> scoreElements = new ArrayList<ScoreElement>();
+        scoreElements.addAll(distinctElements.values()); // insert all distinct ScoreElements
+        LOGGER.debug("Check scoreElements");
+        for (ScoreElement e: scoreElements) {
+            LOGGER.debug(e.getId());
         }
         return scoreElements; //getSimilar(,QueryConfig(qc).setDistanceIfEmpty(QueryConfig.Distance.haversine));
-
     }
 }
